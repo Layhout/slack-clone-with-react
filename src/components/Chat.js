055c34f13a4f5eaee8aa14b73963b29838ae38be
Layhout <85871695+Layhout@users.noreply.github.com>
2021-08-31 16:59:30 +0700

    update

---
+++ src/components/Chat.js
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+
+const Chat = () => {
+    return (
+        <ChatContainer>
+
+        </ChatContainer>
+    )
+}
+
+export default Chat
+
+const ChatContainer = styled.div`
+    flex: 0.7;
+    flex-grow: 1;
+    overFlow-y: scroll;
+`;
